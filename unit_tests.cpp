cb77c68c437c3631c05a9bda5f0e3b7907c41ca1
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-05-24 01:59:38 -0700

    Added Date constructor unit tests

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,6 +1,19 @@
 #include "gtest/gtest.h"
 
+// Meeting Class Unit Tests
 #include "unitTests/Meeting/meeting_construct.hpp"
+
+// GenericTask Class Unit Tests
+
+// Homework Class Unit Tests
+
+// Shopping Class Unit Tests
+
+// Date Class Unit Tests
+#include "unitTests/Date/date_constructor_tests.hpp"
+
+// Time Class Unit Tests
+
 
 int main(int argc, char **argv){
 	::testing::InitGoogleTest(&argc, argv);

5304c1a64fce02d1bbc9c74fcea8ce95cf31cbc3
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-05-29 23:44:42 -0700

    Initial test for ParentTask::title menu test

--- unitTests/ParentTask/parenttask_mutator_tests.hpp
+++ unitTests/ParentTask/parenttask_mutator_tests.hpp
@@ -3,36 +3,60 @@
 
 #include "gtest/gtest.h"
 
+#include <string>
+#include <sstream>
+
 #include "../../Tasks/ParentTask.hpp"
 #include "../../Tasks/Meeting/Meeting.hpp"
 
+// Standard Mutator - No Menu
 TEST(ParentTask_NoMenuMutator, ExpectedInput){
     
     ParentTask* test = new Meeting();
+    string expectedString = "Sample Title - Event in 2 days."
 
-    test->setTitle("Sample Title - Event in 2 days.");
+    test->setTitle(expectedString);
     
-    EXPECT_EQ(test->getTitle(), "Sample Title - Event in 2 days.");
+    EXPECT_EQ(test->getTitle(), expectedString);
 
 }
 
 TEST(ParentTask_NoMenuMutator, NumericalInput){
 
     ParentTask* test = new Meeting();
+    string expectedString = "123456.789"
 
-    test->setTitle("123456.789");
+    test->setTitle(expectedString);
     
-    EXPECT_EQ(test->getTitle(), "123456.789");
+    EXPECT_EQ(test->getTitle(), expectedString);
 }
 
 TEST(ParentTask_NoMenuMutator, EmptyInput){
 
-    ParentTask* test = new Meeting();
+    ParentTask* test = new Meeting("Test Title");
 
     test->setTitle("");
     
     EXPECT_EQ(test->getTitle(), "");
 
+}
+
+// Non-Standard Mutator - With Menu
+TEST(ParentTask_MenuMutator, ExpectedInput){
+
+    string titleInput = "Sample Title - Event in 2 days.";
+
+    istringstream input.str(titleInput);
+    ostringstream output;
+
+    //input.str(expectedString);
+
+    ParentTask* test = new Meeting();
+
+    test->setTitleFromMenu(input, output);
+
+    string expectedOutput("Input new title: " + "Saved...\n");
+    EXPECT_EQ(output.str(), expectedOutput);
 }
 
 #endif

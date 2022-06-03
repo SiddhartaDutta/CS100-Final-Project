225503c9faf8016990524a116bb75c58cfb6f421
jacobcdo <103026639+jacobcdo@users.noreply.github.com>
2022-06-03 10:20:53 -0700

    updated header to match uml diagram

--- Tasks/Shopping/Shopping.hpp
+++ Tasks/Shopping/Shopping.hpp
@@ -1,4 +1,44 @@
 #ifndef __SHOPPING_HPP__
 #define __SHOPPING_HPP__
 
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+#include "../ParentTask.hpp"
+
+class Shopping : public ParentTask {
+
+        private:
+
+                vector<string*> ShoppingList;
+
+        public:
+
+                ShoppingList(vector<string*> slist ){
+                        this-> ShoppingList = slist;
+                }
+
+        /* mutators */
+
+                void setShoppingList(int index, string newItem);
+
+                void addItem(string newItem);
+
+                void removeItem(int index);
+
+                void editTask(istream& input, ostream& output);
+
+
+        /* accessors */
+
+                void printAllInfo();
+
+                vector<string*> getShoppingList();
+
+};
+
 #endif

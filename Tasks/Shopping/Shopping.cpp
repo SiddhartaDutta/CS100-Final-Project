23bcf6bc0f17661bf9212bec8e7e36f748bfc642
jacobcdo <103026639+jacobcdo@users.noreply.github.com>
2022-06-03 10:21:59 -0700

    updated shopping.cpp to update shoppinglist

--- Tasks/Shopping/Shopping.cpp
+++ Tasks/Shopping/Shopping.cpp
@@ -1,1 +1,34 @@
 #include "Shopping.hpp"
+#include "GenericTask.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void Shopping::printAllInfo(){
+        cout << "test\n";
+}
+
+void Shopping::editTask(){
+        cout <<"task edited\n";
+}
+
+vector<string*> Shopping::getShoppingList(){
+        return ShoppingList;
+}
+
+void Shopping::setShoppingList(int index, string newItem){
+        if(index < 0)
+                return;
+
+        vector<string*>::iterator it = ShoppingList.begin();
+        ShoppingList.insert (it+index, &newItem);
+}
+
+void Shopping::addItem(string newItem){
+        ShoppingList.push_back (&newItem);
+}
+
+void Shopping::removeItem(int index){
+        ShoppingList.erase(ShoppingList.begin()+index)
+}

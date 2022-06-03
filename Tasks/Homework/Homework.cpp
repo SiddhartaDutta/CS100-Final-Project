d2e481b2086c64707b5cf4fe4941816ea4253b78
slin182 <slin182@ucr.edu>
2022-06-03 12:24:23 -0700

    Add Homework Task

--- Tasks/Homework/Homework.cpp
+++ Tasks/Homework/Homework.cpp
@@ -1,1 +1,50 @@
 #include "Homework.hpp"
+#include <iostream>
+#include "../../attributeClasses/Date.hpp"
+#include "../../attributeClasses/Time.hpp"
+#include "../ParentTask.hpp"
+
+using namespace std;
+
+void Homework(string Title = "", int month = 0, int day = 0, int year = 0, int DueTimeHour = 0, int DueTimeMin = 0) : ParentTask() {
+        
+    this->Title = Title;
+
+    Date*DueDate = new Date();
+	DueDate->setMonth(month);
+	DueDate->setDay(day);
+	DueDate->setYear(year);
+
+	Time*DueTime = new Time();
+	DueTime->setHour(DueTimeHour);
+	DueTime->setMinute(DueTimeMin);
+
+}
+
+void EditDueDate(int month, int day, int year){
+    DueDate->setMonth(month);
+	DueDate->setDay(day);
+	DueDate->setYear(year);
+}
+
+void EditDueTime(int hour, int minute){
+    DueTime->setHour(hour);
+    DueTime->setMinute(minute);
+}
+
+
+Date* Homework::GetDueDate() const { return DueDate; }
+
+Time* Homework::GetDueTime() const { return DueTime; }
+
+
+void Homework::printAllInfo(){
+    GetDueDate();
+    GetDueTime();
+}
+
+void Homework::editTask(){
+    GetDueDate();
+    GetDueTime();
+}
+
--- Tasks/Homework/Homework.hpp
+++ Tasks/Homework/Homework.hpp
@@ -1,4 +1,37 @@
 #ifndef __HOMEWORK_HPP__
 #define __HOMEWORK_HPP__
 
+#include <vector>
+
+#include "../ParentTask.hpp"
+#include "../../attributeClasses/Date.hpp"
+#include "../../attributeClasses/Time.hpp"
+
+using namespace std;
+
+
+class Homework : public ParentTask{
+
+    private:
+
+        Date* DueDate;
+
+        Time* DueTime;
+
+        vector<ParentTask*> SubTasks;
+
+    public:
+    
+        void Homework(string Title = "", int month = 0, int day = 0, int year = 0, int DueTimeHour = 0, int DueTimeMin = 0) : ParentTask();
+
+        void EditDueDate(int month, int day, int year);
+
+        void EditDueTime(int hour, int minute);
+
+        void printAllInfo();
+	void EditTask();
+		
+        Date* GetDueDate() const;
+	Time* GetDueTime() const;
+}
 #endif

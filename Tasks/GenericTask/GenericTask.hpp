2c63562adbfca70e3037f3f972346fdb252cf0df
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-06-13 18:15:40 -0700

    Created constructor and destructor

--- Tasks/GenericTask/GenericTask.hpp
+++ Tasks/GenericTask/GenericTask.hpp
@@ -1,6 +1,25 @@
 #ifndef __GENERIC_TASKS_HPP__
 #define __GENERIC_TASKS_HPP__
 
+#include "../ParentTask.hpp"
 
+class GenericTask : public ParentTask{
+
+    private:
+        std::string description;
+
+    public:
+
+        // Constructor
+        GenericTask(std::string desc = ""){
+            description = desc;
+        }
+
+        // Destructor
+        ~GenericTask(){}
+
+        
+
+};
 
 #endif

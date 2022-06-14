020b1172c29bfd0bb5c1901b1196686e1908daf9
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-06-13 18:33:40 -0700

    Added missing param to constructor + added mising editTask method

--- Tasks/GenericTask/GenericTask.hpp
+++ Tasks/GenericTask/GenericTask.hpp
@@ -11,7 +11,8 @@ class GenericTask : public ParentTask{
     public:
 
         // Constructor
-        GenericTask(std::string desc = ""){
+        GenericTask(std::string title = "", std::string desc = ""){
+            this->title = title;
             description = desc;
         }
 
@@ -25,6 +26,11 @@ class GenericTask : public ParentTask{
 
         /// Return desc
         std::string getDescription() const;
+
+        /* Mutators */
+
+        /// Runs menu to edit all attributes : follows pattern for universal object manip
+		void editTask(std::istream& input, std::ostream& output);
 
 };
 
